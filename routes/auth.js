93237f84d8586954431486fd2b8ac13d5e0c395a
zeegerrank <zeegerrank@gmail.com>
2022-11-30 22:22:00 +0700

    create Login endpoint in auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express");const router = express.Router();
 
 const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Register endpoint
 router.post("/register", (req, res) => {
@@ -40,6 +40,61 @@ router.post("/register", (req, res) => {
     .catch((e) => {
       res.status(500).send({
         message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+// Login endpoint
+router.post("/login", (req, res) => {
+  // check if email exists
+  User.findOne({ email: req.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(req.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+          // check if password matches
+          if (!passwordCheck) {
+            return res.status(400).send({
+              message: "Passwords does not match 1",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success res
+          res.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          res.status(400).send({
+            message: "Passwords does not match 2",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      res.status(404).send({
+        message: "Email not found",
         e,
       });
     });
