a2f8ff348418927b3b1076a5997b5508b92d8d8f
zeegerrank <zeegerrank@gmail.com>
2022-11-30 15:40:34 +0700

    add index to for heroku service deploy

---
+++ index.js
@@ -0,0 +1,57 @@
+/* For Heroku */ const http = require("http");
+const app = require("./app");
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3010");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+server.listen(port);
+
+// For vercel API deploy
+// const BuiltTime = require("./built-time");
+// module.exports = (req, res) => {
+//   res.setHeader("content-type", "text/plain");
+//   res.send(`
+//     This Serverless Function was built at ${new Date(BuiltTime)}.
+//     The current time is ${new Date()}
+//   `);
+// };
