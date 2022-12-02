5930ffeac27062cd271bc282116463347537f09e
zeegerrank <zeegerrank@gmail.com>
2022-12-02 21:45:09 +0700

    imply basic express-validators to login

--- routes/auth.js
+++ routes/auth.js
@@ -5,83 +5,53 @@ const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 // Register endpoint
-router.post("/register", async (req, res) => {
-  // hash the password
-  // bcrypt
-  //   .hash(req.body.password, 10)
-  //   .then((hashedPassword) => {
-  //     // create a new user instance and collect the data
-  //     const user = new User({
-  //       email: req.body.email,
-  //       password: hashedPassword,
-  //       createdAt: new Date().toUTCString(),
-  //     });
-  const { email, password } = req.body;
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+router.post(
+  "/register",
+  body("email").isEmail().normalizeEmail(),
+  async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
 
-  function registerValidator(email, password) {
-    try {
-      // Detect No Email input
-      if (!email || email == null || email == undefined)
-        throw new Error("Email is required");
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
 
-      // Detect if Email input is real Email
-      const isEmail = body("email".isEmail());
-      if (!isEmail) throw new Error("Email is invalid");
-
-      // Detect No Password input
-      if (!password || password == null || password == undefined)
-        throw new Error("Password is required");
-    } catch (error) {
-      res.status(400).send({
-        message: error,
-      });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
     }
-  }
 
-  const user = new User({
-    email: email,
-    password: hashedPassword,
-    createdAt: new Date().toUTCString(),
-  });
+    // try {
+    //   if (body("email").isEmpty()) {
+    //     throw Error("No Email");
+    //   }
+    //   if (body("email").isNot().isEmail()) {
+    //     throw Error("Email invalid");
+    //   }
+    // } catch (error) {
+    //   console.error(error);
+    //   return;
+    // }
 
-  use.save().then((result) => {
-    res.status(201).send({
-      message: "New User Created Successfully!",
-      result,
+    const user = new User({
+      email,
+      password: hashedPassword,
+      createdAt: new Date().toUTCString(),
     });
-  });
 
-  //   // save the new user
-  //   user
-  //     .save()
-  //     // return success if the new user is added to the database successfully
-  //     .then((result) => {
-  //       res.status(201).send({
-  //         message: "User Created Successfully",
-  //         result,
-  //       });
-  //     })
-  //     // catch error if the new user wasn't added successfully to the database
-  //     .catch((error) => {
-  //       res.status(500).send({
-  //         message: "Error creating user",
-  //         error,
-  //       });
-  //     });
-  // })
-  // // catch error if the password hash isn't successful
-  // .catch((error) => {
-  //   res.status(500).send({
-  //     message: "Password was not hashed successfully",
-  //     error,
-  //   });
-  // });
-});
+    console.log(user);
+
+    res.status(200);
+
+    // try {
+    // } catch (error) {
+    //   res.status(400).send(error);
+    // }
+  }
+);
 
 // Login endpoint
 router.post("/login", (req, res) => {
