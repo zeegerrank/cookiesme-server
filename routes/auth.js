bf687fb249b11636d8df9f2b90518c136dc8dce8
zeegerrank <zeegerrank@gmail.com>
2022-12-02 19:35:24 +0700

    reconfigure register error handler

--- routes/auth.js
+++ routes/auth.js
@@ -1,49 +1,86 @@
-const express = require("express");const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { registerValidator } = require("../validators/register.validator");
+const { body } = require("express-validator");
 
 // Register endpoint
-router.post("/register", registerValidator, (req, res) => {
+router.post("/register", async (req, res) => {
   // hash the password
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hashedPassword) => {
-      // create a new user instance and collect the data
-      const user = new User({
-        email: req.body.email,
-        password: hashedPassword,
-        createdAt: new Date().toUTCString(),
-      });
+  // bcrypt
+  //   .hash(req.body.password, 10)
+  //   .then((hashedPassword) => {
+  //     // create a new user instance and collect the data
+  //     const user = new User({
+  //       email: req.body.email,
+  //       password: hashedPassword,
+  //       createdAt: new Date().toUTCString(),
+  //     });
+  const { email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-      // save the new user
-      user
-        .save()
-        // return success if the new user is added to the database successfully
-        .then((result) => {
-          res.status(201).send({
-            message: "User Created Successfully",
-            result,
-          });
-        })
-        // catch error if the new user wasn't added successfully to the database
-        .catch((error) => {
-          res.status(500).send({
-            message: "Error creating user",
-            error,
-          });
-        });
-    })
-    // catch error if the password hash isn't successful
-    .catch((error) => {
-      res.status(500).send({
-        message: "Password was not hashed successfully",
-        error,
+  function registerValidator(email, password) {
+    try {
+      // Detect No Email input
+      if (!email || email == null || email == undefined)
+        throw new Error("Email is required");
+
+      // Detect if Email input is real Email
+      const isEmail = body("email".isEmail());
+      if (!isEmail) throw new Error("Email is invalid");
+
+      // Detect No Password input
+      if (!password || password == null || password == undefined)
+        throw new Error("Password is required");
+    } catch (error) {
+      res.status(400).send({
+        message: error,
       });
+    }
+  }
+
+  const user = new User({
+    email: email,
+    password: hashedPassword,
+    createdAt: new Date().toUTCString(),
+  });
+
+  use.save().then((result) => {
+    res.status(201).send({
+      message: "New User Created Successfully!",
+      result,
     });
+  });
+
+  //   // save the new user
+  //   user
+  //     .save()
+  //     // return success if the new user is added to the database successfully
+  //     .then((result) => {
+  //       res.status(201).send({
+  //         message: "User Created Successfully",
+  //         result,
+  //       });
+  //     })
+  //     // catch error if the new user wasn't added successfully to the database
+  //     .catch((error) => {
+  //       res.status(500).send({
+  //         message: "Error creating user",
+  //         error,
+  //       });
+  //     });
+  // })
+  // // catch error if the password hash isn't successful
+  // .catch((error) => {
+  //   res.status(500).send({
+  //     message: "Password was not hashed successfully",
+  //     error,
+  //   });
+  // });
 });
 
 // Login endpoint
